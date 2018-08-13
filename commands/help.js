b33fcfcc6fae429c83a6fa1d581038a6d694b0ce
Glitch (croissant-bot) <none>
2018-08-13 00:48:15 +0000

    c!emote search, improvements

--- commands/help.js
+++ commands/help.js
@@ -29,7 +29,7 @@ fs.readdir('./', (err, files) => {
   .addField(prefix + 'face', 'Cool ASCII faces.', true)
   .addField(prefix + 'role [list/remove (role)/get (role)]', 'Obtain selfroles. (start with **+**)', true)
   .addField(prefix + 'purge [1-100]', 'Delete multiple messages in one fell swoop.', true)
-  .addField(prefix + 'emote [url (url)/de (name)]', 'Add custom emoji to the server.', true)
+  .addField(prefix + 'emote [(url/name)/search (q)]', 'Add custom emoji to the server.', true)
   
   message.channel.send(embed).catch(console.error);
 }
