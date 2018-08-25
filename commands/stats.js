ce2809159b711e97e1ba321fac1318e12607f1f4
Glitch (croissant-bot) <none>
2018-08-25 22:26:08 +0000

    Uptime

--- commands/stats.js
+++ commands/stats.js
@@ -1,5 +1,21 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+    // https://stackoverflow.com/a/49921759
+    let totalSeconds = (client.uptime / 1000);
+    let hours = Math.floor(totalSeconds / 3600);
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+    seconds = Math.round(seconds)
+    let uptime = '0 seconds';
+    if (hours > 0) {
+      uptime = `${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+    } else if (minutes > 0) {
+      uptime = `${minutes} minutes and ${seconds} seconds`;
+    } else {
+      uptime = `${seconds} seconds`;
+    }
+  
     let color = message.guild.me.displayColor;
     let embed = new Discord.RichEmbed()
     .setTitle(client.user.username + " Stats")
@@ -7,5 +23,6 @@ exports.run = (client, message, args) => {
     .addField(':satellite_orbital: Servers', client.guilds.size, true)
     .addField(':satellite: Channels', client.channels.size, true)
     .addField(':telescope: Users', client.users.size, true)
+    .addField(':clock10: Uptime', uptime, true)
     message.channel.send(embed).catch(console.error);
 }
