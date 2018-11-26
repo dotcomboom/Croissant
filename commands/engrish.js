cdb68f534a5648687f4302fd092db524384b87bb
dotcomboom <38927017+dotcomboom@users.noreply.github.com>
2018-11-25 21:17:29 -0600

    engrish reimplemented

--- commands/engrish.js
+++ commands/engrish.js
@@ -1,53 +1,37 @@
 const Discord = require("discord.js");
-const translate = require("translate");
+const translate = require("yandex-translate")(process.env.tkey);
 
 exports.run = (client, message, args) => {
-  translate.engine = 'yandex';
-  translate.key = process.env.tkey;
-translate('Hello world', 'es').then(text => {
-  console.log(text);  // Hola mundo
-});
-
-    if (args.length > 0) {
+  if (args.length > 0) {
     let engrish = args.join(' ');
-    translate(engrish, 'japanese').then(text => {
-      engrish = text;
-      translate(engrish, 'chinese').then(text => {
-        engrish = text;
-        translate(engrish, 'arabic').then(text => {
-          engrish = text;
-          translate(engrish, 'english').then(text => {
-            engrish = text;
-            
-            let color = "#ffa500"
-            if (message.guild !== null) {
-              color = message.guild.me.displayColor;
-            }
-            let embed = new Discord.RichEmbed()
+    translate.translate(engrish, { to: 'ru' }, function (err, res) {
+      console.log(engrish);
+      engrish = res.text[0];
+      translate.translate(engrish, { to: 'es' }, function (err, res) {
+        console.log(engrish);
+        engrish = res.text[0];
+        translate.translate(engrish, { to: 'en' }, function (err, res) {
+          console.log(engrish);
+          engrish = res.text[0];
+          let color = "#ffa500"
+          if (message.guild !== null) {
+            color = message.guild.me.displayColor;
+          }
+          let embed = new Discord.RichEmbed()
             .setAuthor(message.author.username)
             .setTitle("Engrish")
             .setColor(color)
             .setDescription(engrish)
-            message.channel.send(embed).catch(console.error);
-            
-          }).catch(err => {
-              console.error(err);
-          });
-        }).catch(err => {
-            console.error(err);
+          message.channel.send(embed).catch(console.error);
         });
-      }).catch(err => {
-        console.error(err);
       });
-    }).catch(err => {
-      console.error(err);
     });
-    } else {
-      let color = '#C1192A';
-      let embed = new Discord.RichEmbed()
+  } else {
+    let color = '#C1192A';
+    let embed = new Discord.RichEmbed()
       .setColor(color)
       .setTitle("Hm?")
       .setDescription("What do you want to make Engrish? " + process.env.prefix + "engrish (something)")
-      message.channel.send(embed).catch(console.error);
-    }
+    message.channel.send(embed).catch(console.error);
+  }
 }
